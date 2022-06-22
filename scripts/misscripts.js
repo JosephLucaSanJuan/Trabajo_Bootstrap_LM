cced04e4c700bdfb4650df48a3c2b24db8418d64
Joseph Luca San Juan Thompson <91873591+JosephLucaSanJuan@users.noreply.github.com>
2022-06-22 08:59:44 +0200

    Add files via upload

---
+++ scripts/misscripts.js
@@ -0,0 +1,16 @@
+/*  */
+
+/* para que funcionen los popovers */
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+
+/* para que funcionen los tooltips */
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
